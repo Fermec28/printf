c14805406a011d932eb2fb81445c728a3f188f82
Jose Medina <fermed28@gmail.com>
2019-03-17 23:34:25 +0000

    [feature] convert number to string

--- holberton.h
+++ holberton.h
@@ -5,8 +5,11 @@
   #include <stdlib.h>*/
 #include <stdarg.h>
 #include <unistd.h>
-
+#include <stdlib.h>
 int _putchar(char *c, unsigned int lenght);
 int _printf(const char *format, ...);
+void reverse_str(char *str, int size);
+char *number_to_string(int number);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* HOLBERTON_H */
