e6a966ee5f200f967561bed7116806d555feaaf8
JaiberS <758@holbertonschool.com>
2019-03-18 21:29:43 +0000

    added condition to not write when p is null

--- printchar.c
+++ printchar.c
@@ -9,9 +9,9 @@ int print_char(va_list valist, options opt)
 	char p = va_arg(valist, int);
 	(void)opt;
 
-	if (p >= 32 &&  p <= 126)
+	if (p >= 32 &&  p <= 126 && p != '\0')
 	{
 		return (write(1, &p, 1));
 	}
-	return (0);
+ 	return (0);
 }
