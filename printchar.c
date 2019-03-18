89fc0bfbd67c6fadd7f06d4c8234dcdcac4878c5
Jose Medina <fermed28@gmail.com>
2019-03-18 04:52:19 +0000

    [Feature] add print string

--- printchar.c
+++ printchar.c
@@ -7,13 +7,14 @@
 void print_char(va_list valist)
 {
 	char p = va_arg(valist, int);
-	if ( p >= 32 &&  p <= 126)
+
+	if (p >= 32 &&  p <= 126)
 	{
 		write(1, &p, 1);
 	}
 	else
 	{
 		printf("Error\n");
-		exit (97);
+		exit(97);
 	}
 }
---
+++ test/16-main.c
@@ -0,0 +1,14 @@
+#include <limits.h>
+#include <stdio.h>
+#include "../holberton.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("%s", "Hola Holberton\n");
+	return (0);
+}
