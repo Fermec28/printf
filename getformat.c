9ccf27645f6aeb75430edd0037ae5ad6101dd435
JaiberS <758@holbertonschool.com>
2019-03-23 23:42:48 +0000

    [Fixes] Betty on Getformat

--- getformat.c
+++ getformat.c
@@ -18,40 +18,23 @@ options getformat(const char *c, int *pos)
 	for (; c[i]; i++)
 	{
 		if (c[i + 1] > '0' && c[i + 1] <= '9')
-		{
 			op.precision =  op.precision + c[i + 1] - 48;
-			continue;
-		}
 		if (c[i + 1] == '-')
-		{
 			op.signn = 1;
-			continue;
-		}
 		if (c[i + 1] == '+')
-		{
 			op.signp = 1;
-			continue;
-		}
 		if (c[i + 1] == ' ')
-		{
 			op.spc = 1;
-			continue;
-		}
 		if (c[i + 1] == '0')
-		{
 			op.zeros = 1;
-			continue;
-		}
 		if (c[i + 1] == '#')
-		{
 			op.hash = 1;
-			continue;
-		}
 		if (c[i + 1] == '.')
-		{
 			op.point = 1;
+		if ((c[i + 1] > '0' && c[i + 1] <= '9') || c[i + 1] == '-'
+		    || c[i + 1] == '+' || c[i + 1] == ' ' || c[i + 1] == '0'
+		    || c[i + 1] == '#' || c[i + 1] == '.')
 			continue;
-		}
 		*pos = i + 1;
 		op.type = c[i + 1];
 		return (op);
